a5e4acd418e7b00f50c43d0f0bab3bfd2b64d023
koleks92 <koleks92@gmail.com>
2023-09-11 19:41:06 +0200

    Ripple effect android/ios

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,43 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 function PrimaryButton (props) {
-    return ( <View>
-        <Text>{ props.children }</Text>
+    function pressHandler() {
+        console.log("Button pressed");
+    }
+    return ( 
+    <View style={styles.buttonOuterContainer} >
+        <Pressable 
+        style={({ pressed }) => pressed ? [styles.pressed, styles.buttonInnerContainerPri] : styles.buttonInnerContainer}
+        onPress={pressHandler}
+        android_ripple={{color: '#3b031f'}}>
+                <Text style={styles.buttonText}>{ props.children }</Text>
+        </Pressable>
     </View>
+
     );
 };
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden',
+
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#52042b",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75,
+
+    }
+});
 
 export default PrimaryButton;
