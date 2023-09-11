569726e9f478ac59219915af92612ddac841bef8
koleks92 <koleks92@gmail.com>
2023-09-11 20:16:40 +0200

    Linear Gradient - BackgroundImage

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground} from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <LinearGradient colors={['#7e0642', '#ddb52f']}style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground 
+      source={require('./assets/images/background.jpg')} 
+      resizeMode='cover'
+      style={styles.rootScreen}
+      imageStyle={styles.backgroundImageStyle}>
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,6 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f',
+  },
+  backgroundImageStyle: {
+    opacity: 0.15
   }
 });
